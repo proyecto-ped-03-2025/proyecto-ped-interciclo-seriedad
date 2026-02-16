26bee2b00f55836b9617899e5a7c547921c6c250
JACR07 <00054525@uca.edu.sv>
2026-02-15 23:46:37 -0600

    add: inclusion de archivos en main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "estructuraParticipantes.h" 
+#include "logicaJuego.h"       
+#include "Menu.h"              
 
 using namespace std;
 int main(){
-    
+    ListaCircular lista; 
+    JuegoPPT juego;      
+    Menu menuObj;        
+
     return 0;
 }
